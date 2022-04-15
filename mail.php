31fc7d9d1dfc8fe58be3e0f245f3f871566c40c9
rsetia23 <61365723+rsetia23@users.noreply.github.com>
2022-04-15 18:38:06 -0400

    changes recaptcha V3

--- mail.php
+++ mail.php
@@ -40,10 +40,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $subject = trim($_POST["subject"]);
     $name = str_replace(array("\r","\n"),array(" "," ") , strip_tags(trim($_POST["name"])));
     $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-    $phone = trim($_POST["phone"]);
+    // $phone = trim($_POST["phone"]);
     $message = trim($_POST["message"]);
     
-    if (empty($name) OR !filter_var($email, FILTER_VALIDATE_EMAIL) OR empty($phone) OR empty($subject) OR empty($message)) {
+    if (empty($name) OR !filter_var($email, FILTER_VALIDATE_EMAIL) OR empty($subject) OR empty($message)) {
       # Set a 400 (bad request) response code and exit
       http_response_code(400);
       echo '<p class="alert-warning">Please complete the form and try again.</p>';
@@ -53,7 +53,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     # Mail content
     $content = "Name: $name\n";
     $content .= "Email: $email\n\n";
-    $content .= "Phone: $phone\n";
+    // $content .= "Phone: $phone\n";
     $content .= "Message:\n$message\n";
 
     # Email headers
